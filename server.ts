f1cae83251ec3f994d02f1ca7a5b04808dff7d79
jesusnoelrg <jesusnoelrg.dev@gmail.com>
2026-07-07 20:33:53 -0700

    chore: migrate core server setup to typescript with tsx.

--- server.ts
+++ server.ts
@@ -3,18 +3,25 @@ const path = require('path');
 const db = require('./sqlite');
 const usuariosRoutes = require('./routes/usuarios');
 const clientesRoutes = require('./routes/clientes');*/
-import express from 'express';
+import express, {type Express, type Request, type Response} from 'express';
 //import UserRoutes from './routes/usuarios.js';
 //import ClientRoutes from './routes/clientes.js';
-import path from 'path';
-const app = express();
+import path from 'path'
+
+
+const app: Express = express();
+
 app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'views')));
-app.get('/', (req, res) => {
+
+app.get('/', (req: Request, res: Response) => {
     res.send('API de ChiliCache');
 });
+
 //app.use('/usuarios', UserRoutes);
 //app.use('/clientes', ClientRoutes);
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
