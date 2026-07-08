2b93e588709dce2b8315ee79c8745295a9058931
jesusnoelrg <jesusnoelrg.dev@gmail.com>
2026-07-07 23:44:25 -0700

    refactor: reorganize project structure into src folder

---
+++ server.js
@@ -0,0 +1,21 @@
+/*const express = require('express');
+const path = require('path');
+const db = require('./sqlite');
+const usuariosRoutes = require('./routes/usuarios');
+const clientesRoutes = require('./routes/clientes');*/
+import express from 'express';
+//import UserRoutes from './routes/usuarios.js';
+//import ClientRoutes from './routes/clientes.js';
+import path from 'path';
+const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'views')));
+app.get('/', (req, res) => {
+    res.send('API de ChiliCache');
+});
+//app.use('/usuarios', UserRoutes);
+//app.use('/clientes', ClientRoutes);
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
