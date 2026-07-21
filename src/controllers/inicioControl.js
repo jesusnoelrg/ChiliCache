b8a20abef3c9e184e2fb0a1fa3fbe03e16490e61
jesusnoelrg <jesusnoelrg.dev@gmail.com>
2026-07-20 18:17:23 -0700

    style: change the design of the button to toggle the aside

--- src/controllers/inicioControl.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    obtenerUsuarios((data) => {
-        document.getElementById('cantidadUsuarios').textContent = data.length;
-    });
-
-    obtenerClientes((data) => {
-        document.getElementById('cantidadClientes').textContent = data.length;
-    });
-});
