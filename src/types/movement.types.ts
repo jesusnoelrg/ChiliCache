aba1d8c4439e836eab773f5e88a427279355c386
jesusnoelrg <jesusnoelrg.dev@gmail.com>
2026-07-24 00:08:02 -0700

    perf: optimize the movement.controller query by preparing it only once for use

--- src/types/movement.types.ts
+++ src/types/movement.types.ts
@@ -1,10 +1,14 @@
 export interface MovementsFilters {
-  type?: string;
-  seller_name?: string;
-  product_name?: string;
-  start_timestamp?: string;
-  end_timestamp?: string;
-  offset?: number;
-  limit?: number;
-  order: 'asc' | 'desc';
+  type: MovementType | null;
+  seller_name: string | null;
+  product_name: string | null;
+  start_timestamp: string | null;
+  end_timestamp: string | null;
+  offset: number;
+  limit: number;
+  order: 'ASC' | 'DESC';
+}
+
+export interface MovementType {
+  type: 'created' | 'restock' | 'sale' | 'cancel';
 }
