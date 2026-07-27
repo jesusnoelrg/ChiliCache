c1a07092e24b5f7e67461f19726b627b3459d18c
jesusnoelrg <jesusnoelrg.dev@gmail.com>
2026-07-26 22:55:46 -0700

    feat: configure 'ejs' to display the variables 'name' and 'logo' in the views

--- src/server.ts
+++ src/server.ts
@@ -25,6 +25,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 app.use(express.json());
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../views')));
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
