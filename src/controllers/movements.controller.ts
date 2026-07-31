2c03385fa003b0a5e85b3ee81620bcc1f6b56570
jesusnoelrg <jesusnoelrg.dev@gmail.com>
2026-07-30 19:21:55 -0700

    refactor(movements.controller): add nextFunction as a parameter to integrate it into the catch

--- src/controllers/movements.controller.ts
+++ src/controllers/movements.controller.ts
@@ -1,4 +1,4 @@
-import type { Request, Response } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 import { MovementsFilters } from '../types/movement.types';
 import { MovementRepository } from '../repositories/movement.repository';
 
@@ -7,7 +7,7 @@ import db from '../config/db'
 const repository = new MovementRepository(db);
 
 export const MovementController = {
-  getMovements: async (req: Request, res: Response) => {
+  getMovements: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const {
         type,
@@ -20,8 +20,8 @@ export const MovementController = {
       const limitNumber = Number(limit || 10);
       const offsetNumber = Number(offset || 0);
 
-      if(isNaN(limitNumber) || limitNumber < 1) return res.status(400).json({"success": false, "message": "El límite debe ser un número mayor que 0."});
-      if(isNaN(offsetNumber)) return res.status(400).json({"success": false, "message": "El offset debe ser un número."});
+      if(isNaN(limitNumber) || limitNumber < 1) return res.status(400).json({success: false, message: "El límite debe ser un número mayor que 0."});
+      if(isNaN(offsetNumber)) return res.status(400).json({success: false, message: "El offset debe ser un número."});
 
       const rawOrder = (order as string)?.toUpperCase();
       const sortOrder = rawOrder === 'ASC' ? 'ASC' : 'DESC';
@@ -39,23 +39,19 @@ export const MovementController = {
 
       const result = repository.findAll(filters);
 
-      if(result.length === 0) return res.status(200).json({"success": true, "message": "No se han encontrado movimientos en el stock."});
+      if(result.length === 0) return res.status(200).json({success: true, message: "No se han encontrado movimientos en el stock."});
 
       return res.status(200).json({
-        "success": true,
-        "metadata": {
-          "limit": limitNumber,
-          "offset": offsetNumber,
-          "count": result.length
+        success: true,
+        metadata: {
+          limit: limitNumber,
+          offset: offsetNumber,
+          count: result.length
         },
-        "data": result
+        data: result
       })
     } catch(err: any){
-      console.log("Error: " + err);
-      return res.status(500).json({
-        "success": false,
-        "message": "[ERROR 500]: Error en la base de datos."
-      })
+      next(err);
     }
   }
 }
