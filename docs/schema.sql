67836e3d22ab61dec41b019e6068c74110524e41
jesusnoelrg <jesusnoelrg.dev@gmail.com>
2026-08-05 23:51:51 -0700

    feat(schema.sql): update the new data model

--- docs/schema.sql
+++ docs/schema.sql
@@ -42,7 +42,32 @@ Table sales_detail {
   id_product integer
 }
 
+Table movements {
+  id integer
+  type varchar
+  old_stock integer
+  new_stock integer
+  created_at timestamp
+  id_product integer
+  id_user integer
+}
+
+Table company {
+  id integer
+  name varchar
+  logo_path varchar
+  tax_id varchar
+  address varchar
+  phone varchar
+  email varchar
+  primary_color varchar
+  secondary_color varchar
+  updated_at timestamp
+}
+
 Ref client_sales: sales.id_client > clients.id
 Ref user_sales: sales.id_user > users.id
 Ref product_sales: products.id < sales_detail.id_product
 Ref sales_details: sales.id < sales_detail.id_sale
+Ref user_movements: movements.id_user > users.id
+Ref product_movements: movements.id_product > products.id
